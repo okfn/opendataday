1979c681ff79a514552b6e6560d9e6e95aaa33e9
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-08 13:56:21 +0000

    [map][m]: finally get round to committing the map of events I have had sitting on my hard disk.

---
+++ js/map.js
@@ -0,0 +1,31 @@
+jQuery(function($) {
+  var url = 'https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=0AqR8dXc6Ji4JdFNQRExkVjI3Q3RtRVZuU1hXRmhlTVE#gid=0';
+  var dataset = new recline.Model.Dataset({
+    url: url,
+    backend: 'gdocs'
+  });
+  var map = new recline.View.Map({
+    el: $('.map'),
+    model: dataset
+  });
+  map.render();
+  dataset.fetch()
+    .done(function() {
+      $('.loading').hide();
+      dataset.records.each(function(record) {
+        if (record.get('latitude')=='' && record.get('place')) {
+          var url = 'http://open.mapquestapi.com/search?format=json&q=' + encodeURIComponent(record.get('place'));
+          console.log(url);
+          $.getJSON(url, function(data) {
+            console.log(data);
+            record.set({
+              latitude: data[0].lat,
+              longitude: data[0].lon
+            });
+          });
+        }
+      });
+    })
+    ;
+});
+
