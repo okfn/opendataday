31321a9ad8b365db4deac74ac087ed38a70ab471
Sam Smith <sam@mintcanary.com>
2017-01-24 17:28:02 +0000

    adjust the scroll offset for smaller header

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,27 @@ jQuery(document).ready(function($){
     element: $('.main-nav')[0]
   })
 
-  // smoothscroll
-  $('.main-nav a').smoothScroll({
-    offset: -90,
-    preventDefault: false
-  });
+  // run test on initial page load
+  checkSize();
+  // run test on resize of the window
+  $(window).resize(checkSize);
+
+  function checkSize(){
+
+    // smoothscroll
+    if ($(".menu").css("display") == "none" ){
+      $('.main-nav a, .banner a').smoothScroll({
+        offset: -20,
+        preventDefault: false
+      });
+    }
+    else {
+      $('.main-nav a, .banner a').smoothScroll({
+        offset: -90,
+        preventDefault: false
+      });
+    }
+
+  }
 
 });
