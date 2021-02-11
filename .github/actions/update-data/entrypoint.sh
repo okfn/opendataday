1c8849deebf06f714994e49a09f1148efc258f90
chris48s <chris.shaw480@gmail.com>
2021-02-11 09:44:46 +0000

    commit and push after update

--- .github/actions/update-data/entrypoint.sh
+++ .github/actions/update-data/entrypoint.sh
@@ -9,6 +9,5 @@ git config user.name "opendataday[bot]"
 git config user.email "actions@users.noreply.github.com"
 git add -A
 timestamp=$(date -u)
-#git commit -m "Update at ${timestamp}" || exit 0
-#git push
-git status
+git commit -m "Update at ${timestamp}" || exit 0
+git push
