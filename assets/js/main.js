bdb8732d8e9996ffac757a146a81f79d37167db1
chris48s <chris.shaw480@gmail.com>
2021-02-24 11:59:49 +0000

    always use same smoothscroll offset: menu is now constant height

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,10 @@ jQuery(document).ready(function($){
   $(window).resize(checkSize);
 
   function checkSize(){
-
     // smoothscroll
-    if ($(".menu").css("display") == "none" ){
-      $('.main-nav a, .banner a').smoothScroll({
-        offset: -20,
-        preventDefault: false
-      });
-    }
-    else {
-      $('.main-nav a, .banner a').smoothScroll({
-        offset: -90,
-        preventDefault: false
-      });
-    }
-
+    $('.main-nav a, .banner a').smoothScroll({
+      offset: -90,
+      preventDefault: false
+    });
   }
 });
