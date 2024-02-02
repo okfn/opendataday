a8d521fbc3f90fa4bd3a4154adabf97d2ae955a0
avdata99 <andres@data99.com.ar>
2024-02-02 09:03:38 -0300

    Stop using expensive service

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 var url = 'https://raw.githubusercontent.com/okfn/opendataday/master/databags/events-2024.json';
-// Open Data Day site custom token
-mapboxgl.accessToken = 'pk.eyJ1Ijoib2tmbiIsImEiOiJjbHM0azFlZjUxYTN4MmtvNTR0d2RxNTZxIn0.XK13ZqhfpDhD2y6ARpo2kw'
+
+mapboxgl.accessToken = 'TO-DO'
 
 var clusterRadius = 50,
   clusterMaxZoom = 14; // Max zoom to cluster points on
