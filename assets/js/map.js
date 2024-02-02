5d35b95e4c34db430fb9cbbe593463a776a738a9
Andres Vazquez <andres@data99.com.ar>
2024-02-02 11:33:56 -0300

    Change map (#405)
    * From mapbox to leaflet + stadia maps

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,77 @@
 var url = 'https://raw.githubusercontent.com/okfn/opendataday/master/databags/events-2024.json';
 
-mapboxgl.accessToken = 'TO-DO'
+var map = L.map('map');
+// on load map event
 
-var clusterRadius = 50,
-  clusterMaxZoom = 14; // Max zoom to cluster points on
+map.on('load', function () {
+  $.getJSON(url).done(function (data) {
+    var events = data.events || [];
+    var geojson = {
+      type: 'FeatureCollection',
+      features: []
+    };
 
-var map = new mapboxgl.Map({
-  container: 'map-container',
-  style: 'mapbox://styles/mapbox/bright-v9',
-  zoom: 1,
-  center: [0, 0],
-  scrollZoom: false
+    events.forEach(function (event) {
+      var lat, lng, title;
+      try {
+        lat = Number(event.latitude);
+        lng = Number(event.longitude);
+        title = truncate(event.event_name, 20);
+      } catch (error) {
+        console.error(error);
+        console.log('Error processing event "' + event.event_name + '"');
+      }
+
+      if (lng && lat && title) {
+
+        // add it to the map
+        let marker = L.marker([lat, lng]).addTo(map);
+        // set up a description for on mouse over
+        marker.bindPopup(getDescription(event));
+
+        geojson.features.push({
+          type: 'Feature',
+          properties: {
+            title: title,
+            icon: "marker",
+            description: getDescription(event)
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          }
+        });
+      }
+    });
+
+    $("#event-number").text(events.length);
+
+    map.on('click', function (e) {
+      var features = map.queryRenderedFeatures(e.point, { layers: ['points'] });
+      if (!features.length) {
+        return;
+      }
+      var feature = features[0];
+      var descriptions = features.map(function (f) { return f.properties.description; });
+      // create a leaflet popup whit descriptions as HTML
+
+      var popup = L.popup()
+        .setLatLng([e.lngLat.lat, e.lngLat.lng]).setContent(descriptions.join('<br><br>')).openOn(map);
+    });
+  });
+
 });
+
+map.setView([0, 0], 4);
+
+var attr = '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>' +
+  '&copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> ' +
+  '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> ' +
+  '&copy; <a href="https://www.openstreetmap.org/about/" target="_blank">OpenStreetMap contributors</a>'
+
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}@2x.jpg', {
+    attribution: attr
+}).addTo(map);
 
 function truncate(str, n) {
   return (str.length > n) ? str.substr(0, n - 1) + '…' : str;
@@ -73,194 +133,7 @@ function getDescription(event) {
   return htmlStr;
 }
 
-map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+// Add control to map
+L.control.scale().addTo(map);
+L.control.zoom({ position: 'topright' }).addTo(map);
 
-map.on('load', function () {
-  $.getJSON(url).done(function (data) {
-    var events = data.events || [];
-    var geojson = {
-      type: 'FeatureCollection',
-      features: []
-    };
-
-    events.forEach(function (event) {
-      var lat, lng, title;
-      try {
-        lat = Number(event.latitude);
-        lng = Number(event.longitude);
-        title = truncate(event.event_name, 20);
-      } catch (error) {
-        console.error(error);
-        console.log('Error processing event "' + event.event_name + '"');
-      }
-
-      if (lng && lat && title) {
-        geojson.features.push({
-          type: 'Feature',
-          properties: {
-            title: title,
-            icon: "marker",  // https://labs.mapbox.com/maki-icons/
-            description: getDescription(event)
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: [lng, lat]
-          }
-        });
-      }
-    });
-
-    $("#event-number").text(events.length);
-
-
-    map.addSource("events", {
-      "type": "geojson",
-      "data": geojson,
-      cluster: true,
-      clusterRadius: clusterRadius,
-      clusterMaxZoom: clusterMaxZoom
-    });
-
-    map.addLayer({
-      "id": "points",
-      "interactive": true,
-      "type": "symbol",
-      "source": "events",
-      "filter": ["!", ["has", "point_count"]],
-      "layout": {
-        "icon-allow-overlap": true,
-        "text-allow-overlap": true,
-        "icon-image": "{icon}-15",
-        "text-size": 15,
-        "text-field": "{title}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-        "text-offset": [0, 0.6],
-        "text-anchor": "top"
-      },
-      "paint": {
-        "text-color": "#333"
-      }
-    });
-
-    map.addLayer({
-      "id": "clusters",
-      "type": "circle",
-      "source": "events",
-      "filter": ["has", "point_count"],
-      "paint": {
-        "circle-color": [
-          "step",
-          ["get", "point_count"],
-          '#00d1ff',     // blue
-          10, '#e077ff', // red from count 10 up
-          20, '#e4ff36'  // yellow from count 20 up
-        ],
-        "circle-radius": 18
-      }
-    });
-
-    map.addLayer({
-      "id": "cluster-count",
-      "type": "symbol",
-      "source": "events",
-      "layout": {
-        "icon-allow-overlap": true,
-        "text-allow-overlap": true,
-        "text-field": "{point_count}",
-        "text-font": [
-          "DIN Offc Pro Medium",
-          "Arial Unicode MS Bold"
-        ],
-        "text-size": 14
-      },
-      "paint": {
-        "text-color": "#fff"
-      }
-    });
-
-    map.on('mousemove', 'points', function (e) {
-      map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'points', function (e) {
-      map.getCanvas().style.cursor = '';
-    });
-    map.on('mousemove', 'clusters', function (e) {
-      map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'clusters', function (e) {
-      map.getCanvas().style.cursor = '';
-    });
-
-    map.on('click', function (e) {
-      var features = map.queryRenderedFeatures(e.point, { layers: ['points', 'clusters'] });
-      if (!features.length) {
-        return;
-      }
-      var feature = features[0];
-
-      if (!feature.properties.cluster) {
-        var descriptions = features.map(function (f) { return f.properties.description; });
-        var popup = new mapboxgl.Popup()
-          .setLngLat(features.length == 1 ? features[0].geometry.coordinates : e.lngLat)
-          .setHTML(descriptions.join('<br><br>'))
-          .addTo(map);
-      } else {
-        // We need to expand a cluster
-        var oldZoom = map.getZoom();
-
-        map.getSource('events').getClusterExpansionZoom(feature.properties.cluster_id, function (error, expansionZoom) {
-          if (error) {
-            throw error;
-          }
-
-          if (expansionZoom <= clusterMaxZoom) {
-            map.flyTo({ center: feature.geometry.coordinates, zoom: expansionZoom });
-          } else {
-            // If we ran out of clustering levels, zoom to fit the individual points
-            map.getSource('events').getClusterLeaves(feature.properties.cluster_id, 9999, 0, function (error, features) {
-              if (error) {
-                throw error;
-              }
-
-              var bounds = pointsToBounds(features);
-              var oldCenter = map.getCenter();
-
-              // Perform a test fit to calculate prospective center and zoom
-              map.fitBounds(bounds, {
-                animate: false,
-                padding: {
-                  top: $('#map-container').height() * 0.1 + $('.main-nav').height(),
-                  bottom: $('#map-container').height() * 0.2,
-                  left: $('#map-container').width() * 0.2,
-                  right: $('#map-container').width() * 0.2
-                }
-              });
-              var newCenter = map.getCenter();
-              var newZoom = map.getZoom();
-
-              // Make sure we stay past the clustering levels
-              newZoom = Math.max(newZoom, clusterMaxZoom + 1);
-
-              // Animate from old view to new view
-              map.jumpTo({ center: oldCenter, zoom: oldZoom });
-              map.flyTo({ center: newCenter, zoom: newZoom });
-            });
-          }
-        });
-      }
-    });
-  });
-
-});
-
-function pointsToBounds(points) {
-  return points.reduce(
-    function (bounds, point) {
-      return bounds.extend(point.geometry.coordinates);
-    },
-    new mapboxgl.LngLatBounds(
-      points[0].geometry.coordinates,
-      points[0].geometry.coordinates
-    )
-  );
-}
