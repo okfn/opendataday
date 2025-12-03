77433c09474ddea5ef0606fc8cae15f585cea170
avdata99 <andres@data99.com.ar>
2025-12-03 09:33:08 -0300

    Fix ETL script

--- etl/2026.py
+++ etl/2026.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from lib import main
 
+# URLs like f'https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=csv&id={SPREADSHEET_ID}&gid={SHEET_ID}'
+# stopped working because google workspace changes their policies.
+# Now we need to use the "publish to web" feature of google sheets.
+# Go to File -> Share -> publish to web -> Link -> Form responses -> Comma-separated values (.csv)
 # Google Spreadsheet main ID
-SPREADSHEET_ID = '1MsZ9-OBdJSxDSZBNZHTdJGecoGwnESPevWAiFh8lUwU'
-# Intrernal sheet ID
+# SPREADSHEET_ID = '1MsZ9-OBdJSxDSZBNZHTdJGecoGwnESPevWAiFh8lUwU'
 SHEET_ID = '1520562486'
-IN_URL = f'https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=csv&id={SPREADSHEET_ID}&gid={SHEET_ID}'
-
+PUBLISH_ID = '2PACX-1vT_98DOFqWc28vkatVNqeb5s-UPWj9g6LvaqX3j8RP6ystwqTeC3P5eGFgnO_USVgYfKTUeuuD2t2VP'
+IN_URL = f'https://docs.google.com/spreadsheets/d/e/{PUBLISH_ID}/pub?gid={SHEET_ID}&single=true&output=csv'
 # If you have permission and want to see (not download) the spreadsheet
 # EDIT_URL = f'https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/edit#gid={SHEET_ID}'
 
